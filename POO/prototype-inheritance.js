c460f2d5ee49ad1d0163701f51cf0f4587672c4a
Edwin R <edwiin.ramz@gmail.com>
2026-02-17 19:47:27 -0600

    palabra reservada super

---
+++ POO/prototype-inheritance.js
@@ -0,0 +1,17 @@
+//estructura del construcotr
+class Animal {
+    constructor (nombre, tipo){
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+    //metodo
+    emitirSonido(){
+        console.log("El animal emite un sonido")
+    }
+}
+//clase constructora de perro va a extender de clase Animal
+class Perro extends Animal{
+    constructor(nombre, tipo, raza){
+        super()
+    }
+}

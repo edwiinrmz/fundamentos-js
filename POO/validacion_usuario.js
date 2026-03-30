7d12002cf2ef05bde567fea2d5b4393f3f721b3a
Edwin R <edwiin.ramz@gmail.com>
2026-03-30 15:06:33 -0600

    instrucciones del reto

---
+++ POO/validacion_usuario.js
@@ -0,0 +1,11 @@
+/*
+Requerimientos  del reto:
+
+1. El usuario debe poder ingresar su usuario y contraseña.
+2. El usuario debe ser capaz de validar si el usuario y constraseña 
+ingresados por el usuario existen en la base de datos.
+3. Si el usuario y contraseña son correctos, el sistema debe mostrar 
+un mensaje de bienvenida y mostrar el timeline del usuario. 
+4. Si el usuario y contraseña son incorrectos, el sistema 
+debe mostrar un mensaje de error y no mostrar ningun timeline.
+*/
